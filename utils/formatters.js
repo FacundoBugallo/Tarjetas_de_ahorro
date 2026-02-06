e5d3dba2c4a914215802a52edc7b774ced5d7e5f
Facundo Bugallo <113198148+FacundoBugallo@users.noreply.github.com>
2026-02-05 21:27:40 -0300

    Make header data-driven

---
+++ utils/formatters.js
@@ -0,0 +1,9 @@
+export const formatCurrency = (value) => {
+  return new Intl.NumberFormat('es-CO', {
+    style: 'currency',
+    currency: 'COP',
+    maximumFractionDigits: 0,
+  }).format(value);
+};
+
+export const clampPercentage = (value) => Math.min(100, Math.max(0, value));
